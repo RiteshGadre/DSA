4670c046f5bb013824f355d52794458c94781f3a
Ritesh Gadre <61073916+RiteshGadre@users.noreply.github.com>
2025-12-10 13:36:52 +0530

    Time: 7 ms (63.95%) | Memory: 11.8 MB (65.45%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int left = 0;
-        int right = 0;
-        int mx = s.size() ? 1 : 0;
+        int left = 0, right= 0, mx = 0, n = s.size();
         unordered_map<char, int> map;
 
         while(right < n) {
-            if(map.find(s[right]) == map.end()) {
-                map[s[right]] = right;
+            map[s[right]]++;
+
+            if(map[s[right]] > 1) {
+                while(s[left] != s[right]) {
+                    map[s[left++]]--;
+                    mx = max(mx, right-left+1);
+                }
+                map[s[left++]]--;
+                
             }
-            else{
-                mx = max(right-left, mx);
-                left = max(map[s[right]]+1, left);
-                map[s[right]] = right;
-            }
-            cout << s[right] << ' ' << mx << ' ' << left << ' ' << right << endl;
             right++;
-            
         }
-        mx = max(right-left, mx);
-        
+        mx = max(mx, right-left);
         return mx;
     }
 };
-/*
-abcabcbb
-
-
-
-*/
